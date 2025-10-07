6db3d998dd38b911c9403500a6b3cfe34c0a54ac
Tayyab Khan <97843826+easy2-code@users.noreply.github.com>
2025-10-07 11:58:55 +0500

    Ready for deployment...

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Ecommerce-part-1">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
