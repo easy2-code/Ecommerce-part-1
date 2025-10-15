641177061d35cdd6f3b97b56a3bd214a32b74519
Tayyab Khan <97843826+easy2-code@users.noreply.github.com>
2025-10-15 14:18:10 +0500

    Config Redux Store...

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,13 @@ import React from "react";
 import HeroSection from "../components/HeroSection";
 import PromoSection from "../components/PromoSection";
 import ProductLayout from "../components/ProductLayout";
+import { useSelector } from "react-redux";
 
 export default function Home() {
+  // Verify Configuration of redux
+  const cart = useSelector((state) => state.cart);
+  console.log("Redux Cart State:", cart);
+
   return (
     <div>
       <HeroSection />
