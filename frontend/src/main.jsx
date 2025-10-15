5fbd4eacf4d1c5e7abfd7890d5e247cc85dd39b3
Tayyab Khan <97843826+easy2-code@users.noreply.github.com>
2025-10-15 14:19:01 +0500

    Config Redux Store....

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux"; // 🧩 import Provider
-import { store } from "./redux/store"; // 🧠 import our store
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import "./index.css";
 import App from "./App.jsx";
 
